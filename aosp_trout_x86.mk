c6cd7e6c8916cdac09d465abf89c148a592aa000
Enrico Granata <egranata@google.com>
2020-03-20 14:00:52 -0700

    Add aosp_trout_x86 target
    This is currently targeting crosvm and can be used for
    local testing and feature development
    
    It is not a deliverable for the virtualization team and
    lacks several key features specific to automotive
    
    Bug: 152076925
    Test: build, then run
          acloud create --local-image --local-instance --hw-property resolution:1920x1024,dpi:160
          If this is the first time one runs this command, follow the steps and then reboot
          the machine and run the acloud command again
          Android Automotive should come up in a VNC window after a while
    
    Change-Id: I3ceffc50d05a1a5a70b97e32fa60b2b9fddcd9c3

---
+++ aosp_trout_x86.mk
@@ -0,0 +1,39 @@
+#
+# Copyright (C) 2020 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Vehicle HAL
+# TODO(egranata, chenhaosjtuacm): this does not work on crosvm for now
+# LOCAL_VHAL_PRODUCT_PACKAGE := android.hardware.automotive.vehicle@2.0-virtualization-service android.hardware.automotive.vehicle@2.0-virtualization-grpc-server
+# PRODUCT_PROPERTY_OVERRIDES += \
+# 	ro.vendor.vehiclehal.server.cid=2 \
+# 	ro.vendor.vehiclehal.server.port=9210 \
+# BOARD_SEPOLICY_DIRS += device/google/trout/sepolicy/vendor/google
+
+$(call inherit-product, device/google/cuttlefish/vsoc_x86/auto/device.mk)
+
+PRODUCT_COPY_FILES += \
+    device/google/trout/product_files/odm/ueventd.rc:$(TARGET_COPY_OUT_ODM)/ueventd.rc \
+
+# TODO(egranata): this should not be needed
+# PRODUCT_COPY_FILES += \
+#    device/google/trout/product_files/vendor/etc/input-port-associations.xml:$(TARGET_COPY_OUT_VENDOR)/etc/input-port-associations.xml \
+
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.hardware.type=automotive \
+
+PRODUCT_NAME := aosp_trout_x86
+PRODUCT_DEVICE := vsoc_x86
+PRODUCT_MODEL := x86 trout
