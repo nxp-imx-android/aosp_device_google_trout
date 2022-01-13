21ef54d53d06e343677610c1212054dbb7777226
Hao Chen <chenhaosjtuacm@google.com>
2022-01-13 13:58:50 -0800

    Add EVS sample app to the Trout build
    Test: Boot and run evs app
    Bug: 214100438
    Change-Id: If8d82938731b8ab6b819ec70de9508fb0b7f2e4d

--- aosp_trout_common.mk
+++ aosp_trout_common.mk
@@ -66,7 +66,11 @@ LOCAL_EVS_PRODUCT_COPY_FILES ?= \
     device/google/trout/product_files/vendor/etc/automotive/evs/evs_configuration_override.xml:$(TARGET_COPY_OUT_VENDOR)/etc/automotive/evs/evs_configuration_override.xml \
 
 LOCAL_EVS_PROPERTIES ?= persist.automotive.evs.mode=1
-ENABLE_EVS_SAMPLE := true
+LOCAL_EVS_RRO_PACKAGE_OVERLAYS ?= TroutEvsOverlay
+ENABLE_EVS_SAMPLE ?= true
+ENABLE_CAREVSSERVICE_SAMPLE ?= true
+
+PRODUCT_PACKAGES += $(LOCAL_EVS_RRO_PACKAGE_OVERLAYS)
 
 PRODUCT_COPY_FILES += \
     ${LOCAL_EVS_PRODUCT_COPY_FILES} \
